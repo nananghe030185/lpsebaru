a16e6c46518ddc39a7159838b645b64f6edb84a4
Nanang Hermawan <abah.daaim@gmail.com>
2026-04-02 15:28:17 +0700

    rename

--- database/migrations/4000_01_01_000000_create_users_table.php
+++ database/migrations/4000_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->string('password');
             $table->string('email')->unique();
+            $table->date('email_verified_at')->nullable();
             $table->integer('group_id')->default(3);
             $table->boolean('status')->default(false);
             $table->integer('system_reserve')->default(0);
