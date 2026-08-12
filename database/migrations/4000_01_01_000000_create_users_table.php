e305c205324ce3cd6cba5fd9aa67f56002027e55
Nanang Hermawan <abah.daaim@gmail.com>
2026-08-12 15:55:27 +0700

    db

--- database/migrations/4000_01_01_000000_create_users_table.php
+++ database/migrations/4000_01_01_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->string('password');
-            $table->string('two_factor_secret')->nullable();
-            $table->string('two_factor_recovery_codes')->nullable();
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+             $table->timestamp('two_factor_confirmed_at')->nullable();
             $table->string('email')->unique();
             $table->date('email_verified_at')->nullable();
             $table->integer('group_id')->default(3);
