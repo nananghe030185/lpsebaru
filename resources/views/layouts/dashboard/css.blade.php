92bad25d9c0faabe66689a983ee319127497f236
Nanang Hermawan <abah.daaim@gmail.com>
2026-08-12 16:50:17 +0700

    dba

--- resources/views/layouts/dashboard/css.blade.php
+++ resources/views/layouts/dashboard/css.blade.php
@@ -1,27 +1,27 @@
     <!-- Font Awesome-->
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/fontawesome.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/fontawesome.css') }}">
     <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/icofont.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/icofont.css') }}">
     <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/themify.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/themify.css') }}">
     <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/flag-icon.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/flag-icon.css') }}">
     <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/feather-icon.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/feather-icon.css') }}">
     <!-- Plugins css start-->
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/slick.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/slick-theme.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/scrollbar.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/slick.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/slick-theme.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/scrollbar.css') }}">
     <!-- Plugins css Ends-->
     @yield('css')
     <!-- Bootstrap css-->
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/bootstrap.css') }}">
     <!-- App css-->
     @vite(['resources/assets/scss/style.scss'])
 
-    <link id="color" rel="stylesheet" href="{{ minify('/css/color-1.css') }}" media="screen">
+    <link id="color" rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}" media="screen">
     <!-- Responsive css-->
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/responsive.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
 
-    <link rel="stylesheet" type="text/css" href="{{ minify('/css/vendors/toastr.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/toastr.min.css')}}">
 
--- resources/views/layouts/dashboard/scripts.blade.php
+++ resources/views/layouts/dashboard/scripts.blade.php
@@ -1,26 +1,26 @@
  <!-- latest jquery-->
  <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
  <!-- Bootstrap js-->
- <script src="{{ minify('/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+ <script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
  <!-- feather icon js-->
- <script src="{{ minify('/js/icons/feather-icon/feather.min.js') }}"></script>
- <script src="{{ minify('/js/icons/feather-icon/feather-icon.js') }}"></script>
+ <script src="{{ asset('assets/js/icons/feather-icon/feather.min.js') }}"></script>
+ <script src="{{ asset('assets/js/icons/feather-icon/feather-icon.js') }}"></script>
  <!-- scrollbar js-->
  <script src="{{ asset('assets/js/scrollbar/simplebar.min.js') }}"></script>
- <script src="{{ minify('/js/scrollbar/custom.js') }}"></script>
+ <script src="{{ asset('assets/js/scrollbar/custom.js') }}"></script>
  <!-- Sidebar jquery-->
- <script src="{{ minify('/js/config.js') }}"></script>
+ <script src="{{ asset('assets/js/config.js') }}"></script>
  <!-- Plugins JS start-->
- <script src="{{ minify('/js/sidebar-menu.js') }}"></script>
- <script src="{{ minify('/js/sidebar-pin.js') }}"></script>
- <script src="{{ minify('/js/slick/slick.min.js') }}"></script>
- <script src="{{ minify('/js/slick/slick.js') }}"></script>
- <script src="{{ minify('/js/header-slick.js') }}"></script>
+ <script src="{{ asset('assets/js/sidebar-menu.js') }}"></script>
+ <script src="{{ asset('assets/js/sidebar-pin.js') }}"></script>
+ <script src="{{ asset('assets/js/slick/slick.min.js') }}"></script>
+ <script src="{{ asset('assets/js/slick/slick.js') }}"></script>
+ <script src="{{ asset('assets/js/header-slick.js') }}"></script>
  @yield('scripts')
- <script src="{{ minify('/js/script.js') }}"></script>
- <script src="{{ minify('/js/script1.js') }}"></script>
- <script src="{{ minify('/js/theme-customizer/customizer.js') }}"></script>
- <script src="{{ minify('/js/toastr.min.js') }}"></script>
+ <script src="{{ asset('assets/js/script.js') }}"></script>
+ <script src="{{ asset('assets/js/script1.js') }}"></script>
+ <script src="{{ asset('assets/js/theme-customizer/customizer.js') }}"></script>
+ <script src="{{ asset('assets/js/toastr.min.js') }}"></script>
 
 <!-- Status Update-->
  <script>
