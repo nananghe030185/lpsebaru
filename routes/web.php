09a74ed0c827f0f34523d2b29e33583a640bcc3e
Nanang Hermawan <abah.daaim@gmail.com>
2026-08-12 16:04:11 +0700

    db

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Artisan;
 Route::get('/install', function(){
     Artisan::call('migrate:fresh --seed');
     Artisan::call('optimize:clear');
+    return 'Done ...<a href="' . route('dashboard') . '">Go to Dashboard</a> <p>'. Artisan::output() .'</p>';
 });
 
 Route::get('/', function () {
