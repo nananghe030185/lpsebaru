5823245d671ee770f13d25b1305868fc5a938c20
Nanang Hermawan <abah.daaim@gmail.com>
2026-08-12 15:48:41 +0700

    db

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Artisan;
 
 
 Route::get('/install', function(){
-    Artisan::call('make:session-table');
-    Artisan::call('session:table');
-    Artisan::call('migrate');
+    Artisan::call('migrate:fresh --seed');
     Artisan::call('optimize:clear');
 });
 
