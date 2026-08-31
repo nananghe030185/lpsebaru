099e40bb3c4eba77bdb8480feb9c8caf074a004f
Nanang Hermawan <abah.daaim@gmail.com>
2026-08-31 12:24:43 +0700

    css

--- resources/views/layouts/dashboard/css.blade.php
+++ resources/views/layouts/dashboard/css.blade.php
@@ -19,7 +19,7 @@
 
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
     <!-- App css-->
-    @vite(['resources/assets/scss/style.scss'])
+    {{-- @vite(['resources/assets/scss/style.scss']) --}}
 
     <link id="color" rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}" media="screen">
     <!-- Responsive css-->
